5b5f1267fb5b0f187519f77e649f7db2c1303692
LucCornu1 <LucCORNU.tf@outlook.fr>
2021-02-17 18:54:58 +0100

    Blueprints

---
+++ Source/CORNU_3DPlateformer/CORNU_3DPlateformer.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CORNU_3DPlateformer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CORNU_3DPlateformer, "CORNU_3DPlateformer" );
